f611a721150e514406598d13306d8feddafad3b8
Will Johnson <williamterryjohnsonjr@gmail.com>
2017-04-19 21:26:33 -0400

    Began setting up sequelize and postgreSQL

--- index.js
+++ index.js
@@ -2,6 +2,31 @@ var express = require('express');
 
 var app = express();
 var port = process.env.PORT || 9000;
+var sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql'|'sqlite'|'postgres'|'mssql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+});
+
+var dbAccessString = require('./secrets');
+
+// Or you can simply use a connection uri
+var sequelize = new Sequelize(dbAccessString);
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
+
+
 var nav = [{
     Link: '/',
     Text: 'Home'
